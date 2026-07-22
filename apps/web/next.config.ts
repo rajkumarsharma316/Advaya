f063e4d1e47e7d6935ab1cb796951e6cfda3601d
rajkumarsharma316 <rajsharma970417@gmail.com>
2026-07-22 15:09:04 +0530

    fix: remove invalid turbo config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      root: 'c:/Users/Victus/Documents/getstarted/Advaya/apps/web',
-    }
-  },
 };
 
 export default nextConfig;
